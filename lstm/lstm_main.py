877e1c4f3fa673f6ced5038b08233365ab7f71a3
maxherget <hergetmax@icloud.com>
2024-05-16 21:17:34 +0200

    added LSTM

--- lstm/lstm_main.py
+++ lstm/lstm_main.py
@@ -100,9 +100,112 @@ from torch.utils.data import DataLoader
 batch_size = 16
 
 train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
 
 for _, batch in enumerate(train_loader):
     x_batch, y_batch = batch[0].to(device), batch[1].to(device)
     print(x_batch.shape, y_batch.shape)
     break
+
+
+class LSTM(nn.Module):
+    def __init__(self, input_size, hidden_size, num_stacked_layers):
+        super().__init__()
+        self.hidden_size = hidden_size
+        self.num_stacked_layers = num_stacked_layers
+
+        self.lstm = nn.LSTM(input_size, hidden_size, num_stacked_layers, batch_first=True)
+        self.fc = nn.Linear(hidden_size, 1)
+
+    def forward(self, x):
+        batch_size = x.size(0)
+        h0 = torch.zeros(self.num_stacked_layers, batch_size, self.hidden_size).to(device)
+        c0 = torch.zeros(self.num_stacked_layers, batch_size, self.hidden_size).to(device)
+
+        out, _ = self.lstm(x, (h0, c0))
+        out = self.fc(out[:, -1, :])
+        return out
+
+
+model = LSTM(1, 4, 1)
+model.to(device)
+print(model)
+
+learning_rate = 0.001
+num_epochs = 10
+loss_function = nn.MSELoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+
+
+def train_one_epoch():
+    model.train(True)
+    print(f'Epoch: {epoch + 1}')
+    running_loss = 0.0
+
+    for batch_index, batch in enumerate(train_loader):
+        x_batch, y_batch = batch[0].to(device), batch[1].to(device)
+        y_batch = y_batch.unsqueeze(1) #fixt das problem mit unterschiedlichen dimensionen
+
+
+        output = model(x_batch)
+        loss = loss_function(output, y_batch)
+        running_loss += loss.item()
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        if batch_index % 100 == 99:  # print every 100 batches
+            avg_loss_across_batches = running_loss / 100
+            print('Batch {0}, avg. Loss: {1:.3f}'.format(batch_index + 1,
+                                                         avg_loss_across_batches))
+            running_loss = 0.0
+    print()
+
+
+def validate_one_epoch():
+    model.train(False)
+    running_loss = 0.0
+
+    for batch_index, batch in enumerate(test_loader):
+        x_batch, y_batch = batch[0].to(device), batch[1].to(device)
+        y_batch = y_batch.unsqueeze(1)  # Gleiche Anpassung wie oben.
+
+        with torch.no_grad():
+            output = model(x_batch)
+            loss = loss_function(output, y_batch)
+            running_loss += loss.item()
+
+        avg_loss_across_batches = running_loss / len(test_loader)
+
+    print('Valid. Loss: {0:.3f}'.format(avg_loss_across_batches)) #durchschnittliche Verlust auf dem Validierungsdatensatz
+    print('###########################################')
+    print()
+
+
+for epoch in range(num_epochs):
+    train_one_epoch()
+    validate_one_epoch()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
